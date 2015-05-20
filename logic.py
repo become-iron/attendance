33142ea3df3fb4f99ec7ad876589ce6c618149d6
become-iron <nikitaburkov@yandex.ru>
2015-05-20 21:08:12 +0300

    Пара новых функций

--- logic.py
+++ logic.py
@@ -29,6 +29,8 @@ def add_group(group):
     ДОБАВИТЬ ГРУППУ
     Принимает:
         group (str) - номер группы
+    Возвращает:
+        (bool) - успешность операции
     """
     if not exists(DBS_PATH):  # если нет папки с группами, создать
         mkdir(DBS_PATH)
@@ -54,9 +56,7 @@ def add_students(group, students):
         1 - повышенные права
         2 - суперпользователь
     Возвращает:
-        (bool):
-            True - операция успешна
-            False - не выполнена
+        (bool) - успешность операции
     """
     base = _read_db(group)
     for student in students:
@@ -66,6 +66,30 @@ def add_students(group, students):
             right = 0
         base['students'].update({student[0]: (student[1], right)})
     return True if _save_db(group=group, data=base) else False
+
+
+def add_lessons(group, semester, lessons):
+    """
+    ДОБАВИТЬ СЕМЕСТР, ДАТЫ ЗАНЯТИЙ
+    Принимает:
+        group (str) - номер группы
+        semester (str) - номер группы
+        lessons (tuple) - список дат занятий
+    Возвращает:
+        (bool) - успешность операции
+    ПРИМЕЧАНИЕ: #TODO
+    """
+    base = _read_db(group)
+    base['semesters'].update({semester: lessons})
+    return True if _save_db(group=group, data=base) else False
+
+
+def update_student():
+    """
+    ОБНОВИТЬ ДАННЫЕ СТУДЕНТА
+    """
+    pass
+    # TODO
 
 
 def get_groups():
@@ -82,13 +106,27 @@ def get_semesters(group):
     """
     ПОЛУЧИТЬ СПИСОК СЕМЕСТРОВ В ГРУППЕ
     Принимает:
-        (str) - номер группы
+        group (str) - номер группы
     Возвращает:
         (tuple) - список семестров
             (str) - каждый элемент
     """
     base = _read_db(group)
     return tuple(base['semesters'].keys())
+
+
+def get_lessons(group, semester):
+    """
+    ПОЛУЧИТЬ СПИСОК ЗАНЯТИЙ (ДАТЫ) В СЕМЕСТРЕ
+    Принимает:
+        group (str) - номер группы
+        semester (str) - номер семестра
+    Возвращает:
+        (tuple) - список дат занятий
+    """
+    base = _read_db(group)
+    return base['semesters'][semester]
+    # WARN: проверить работоспособность
 
 
 # ВНУТРЕННИЕ ФУНКЦИИ
