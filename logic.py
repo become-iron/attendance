4c591ca13f96eaa574bc1132632adb5ab0782be2
become-iron <nikitaburkov@yandex.ru>
2015-05-20 20:22:11 +0300

    Рефакторинг, исправление ошибок

--- logic.py
+++ logic.py
@@ -10,6 +10,7 @@ DB_STRUCTURE = \
         'semesters': {},
         'attendance': {}
     }
+db_temp = ''
 
 
 class WrongDataError(Exception):
@@ -24,32 +25,40 @@ class BaseNotFoundError(Exception):
 
 
 def add_group(group):
-    if isinstance(group, str) or isinstance(group, int):
-        group = str(group)
-    else:
-        raise WrongDataError('Параметр group должен быть int или str')
-    # если нет папки с группами, создать
-    if not exists(DBS_PATH):
+    """
+    ДОБАВИТЬ ГРУППУ
+    Принимает:
+        group (str) - номер группы
+    """
+    if not exists(DBS_PATH):  # если нет папки с группами, создать
         mkdir(DBS_PATH)
-    # создать пустой файл для группы
-    path = DBS_PATH + '/' + group
-    if exists(path):
-        return False
-    with open(path, 'w') as db:
-        # записать в файл основную структуру базы
-        _ = json.dumps(DB_STRUCTURE)
-        db.write(_)
-        db.close()
-    return True
+    return True if _save_db(group, DB_STRUCTURE) else False
 
 
 def add_students(group, students):
     """
-    ((pers_num, name, right),
-    ...
-    (pers_num, name, right))
+    ДОБАВИТЬ СТУДЕНТОВ
+    Принимает:
+        group (str) - номер группы
+        students (tuple) - студенты (таб. номера, имена, права)
+
+        Пример параметра students:
+        ((pers_num, name, right),
+        ...
+        (pers_num, name, right))
+
+        Права необязательно указывать, по умолчанию присвоится код 0
+
+        Коды прав пользователей:
+        0 - обычный пользователь
+        1 - повышенные права
+        2 - суперпользователь
+    Возвращает:
+        (bool):
+            True - операция успешна
+            False - не выполнена
     """
-    base = _decode_db(group)
+    base = _read_db(group)
     for student in students:
         try:
             right = student[2]
@@ -66,7 +75,6 @@ def get_groups():
         (list) - список групп
             (str) - каждый элемент
     """
-    # return tuple(map(lambda x: x.replace('.db', ''), listdir(DBS_PATH)))
     return listdir(DBS_PATH)
 
 
@@ -74,38 +82,38 @@ def get_semesters(group):
     """
     ПОЛУЧИТЬ СПИСОК СЕМЕСТРОВ В ГРУППЕ
     Принимает:
-        (str) или (int) - номер группы
+        (str) - номер группы
     Возвращает:
         (tuple) - список семестров
             (str) - каждый элемент
     """
-    base = _decode_db(group)
+    base = _read_db(group)
     return tuple(base['semesters'].keys())
 
 
-def _decode_db(group):
-    if isinstance(group, str) or isinstance(group, int):
-        group = str(group)
-    else:
-        raise WrongDataError('Параметр group должен быть int или str')
+# ВНУТРЕННИЕ ФУНКЦИИ
+def _path(file):
+    return DBS_PATH + '/' + file
+
+
+def _read_db(group):
+    global db_temp
     path = _path(group)
     if not exists(path):
         raise BaseNotFoundError
     base = open(path, 'r').read()
+    db_temp = base
     return json.loads(base)
 
 
 def _save_db(group, data):
-    if isinstance(group, str) or isinstance(group, int):
-        group = str(group)
-    else:
-        raise WrongDataError('Параметр group должен быть int или str')
     path = _path(group)
-    with open(path, 'w') as file:
-        file.write(json.dumps(data))
-        file.close()
+    try:
+        with open(path, 'w') as file:
+            file.write(json.dumps(data))
+            file.close()
+    except ValueError:
+        with open(path, 'w') as base:
+            base.write(db_temp)
+            base.close()
     return True
-
-
-def _path(file):
-    return DBS_PATH + '/' + file
